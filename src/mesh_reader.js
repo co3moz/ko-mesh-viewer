dba715079e1ce4cb415fb3aff433c6a7063d52cc
Doğan Derya <doganderya59@gmail.com>
2019-04-22 00:57:01 +0300

    bug fix

--- src/mesh_reader.js
+++ src/mesh_reader.js
@@ -8,17 +8,17 @@ function readN3PMesh(data) {
 
   const collapseCount = data.getInt32(offset, true);
   const indexChangeCount = data.getInt32(offset + 4, true);
-  const verticeMaxCount = data.getInt32(offset + 8, true);
-  const indiceMaxCount = data.getInt32(offset + 12, true);
+  const verticeCount = data.getInt32(offset + 8, true);
+  const faceCount = data.getInt32(offset + 12, true);
   const verticeMinCount = data.getInt32(offset + 16, true);
-  const indiceMinCount = data.getInt32(offset + 20, true);
+  const faceMinCount = data.getInt32(offset + 20, true);
   offset += 24;
 
 
-  if (verticeMaxCount > 0x8000 || indiceMaxCount > 0x8000) throw new Error('not allowed');
+  if (verticeCount > 0x8000 || faceCount > 0x8000) throw new Error('not allowed');
 
   const vertices = [];
-  for (let i = 0; i < verticeMaxCount; i++) {
+  for (let i = 0; i < verticeCount; i++) {
     vertices.push({
       x: data.getFloat32(offset, true),
       y: data.getFloat32(offset + 4, true),
@@ -36,15 +36,98 @@ function readN3PMesh(data) {
 
   const faces = [];
 
-  for (let i = 0; i < indiceMaxCount; i++) {
+  for (let i = 0; i < faceCount; i++) {
     const v = data.getUint16(offset, true);
     offset += 2;
 
     faces.push(v);
   }
 
+
+  const collapses = [];
+  for (let i = 0; i < collapseCount; i++) {
+    collapses.push({
+      indiceLoseCount: data.getInt32(offset, true),
+      indiceChangeCount: data.getInt32(offset + 4, true),
+      verticeLoseCount: data.getInt32(offset + 8, true),
+      indexChange: data.getInt32(offset + 12, true),
+      collapseTo: data.getInt32(offset + 16, true),
+      shouldCollapse: data.getInt32(offset + 20, true),
+    });
+
+    offset += 24;
+  }
+
+
+  const indexChanges = [];
+
+  for (let i = 0; i < indexChangeCount; i++) {
+    indexChanges.push(data.getInt32(offset, true));
+    offset += 4;
+  }
+
+  const LODCtrlCount = data.getInt32(offset, true);
+  offset += 4;
+
+  const LODCtrls = [];
+
+  for (let i = 0; i < LODCtrlCount; i++) {
+    LODCtrls.push({
+      fDist: data.getFloat32(offset, true),
+      verticeCount: data.getInt32(offset + 4, true)
+    });
+
+    offset += 8;
+  }
+
+  let i = 0;
+  let c = 0;
+  let iVerticeCount = 0;
+  let iIndiceCount = 0;
+
+  while (LODCtrls[i] && LODCtrls[i].verticeCount > iVerticeCount) {
+    if (c >= collapseCount) break;
+    let collapse = collapses[c];
+    if (collapse.verticeLoseCount + iVerticeCount > LODCtrls[i].verticeCount) break;
+
+    iVerticeCount += collapse.verticeLoseCount;
+    iIndiceCount += collapse.indiceLoseCount;
+
+    let min = collapse.indexChange;
+    let max = min + collapse.indiceChangeCount;
+
+    for (let j = min; j < max; j++) {
+      faces[indexChanges[j]] = iVerticeCount - 1;
+    }
+
+    c++;
+  }
+
+  while (c < collapseCount && collapses[c].shouldCollapse) {
+    let collapse = collapses[c];
+
+    iVerticeCount += collapse.verticeLoseCount;
+    iIndiceCount += collapse.indiceLoseCount;
+
+    let min = collapse.indexChange;
+    let max = min + collapse.indiceChangeCount;
+
+    for (let j = min; j < max; j++) {
+      faces[indexChanges[j]] = iVerticeCount - 1;
+    }
+
+    c++;
+  }
+
   return ({
     meshName,
+    count: {
+      collapse: collapseCount,
+      indexChange: indexChangeCount,
+      vertice: { min: verticeMinCount, max: verticeCount },
+      indice: { min: faceMinCount, max: faceCount }
+    },
+
     vertices,
     faces
   })
